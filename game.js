1e3d6a62c5e03c439fc2d37baf765c9bcd084844
yuu3232 <yusei82817@aol.com>
2026-09-04 18:24:42 +0900

    refactor: move humanoid entity creation out of game.js

--- game.js
+++ game.js
@@ -15,6 +15,7 @@ import { createFieldController } from "./field.js";
 import { createPlayerController } from "./player.js";
 import { animateHumanoid } from "./animation.js";
 import { createInputController } from "./input.js";
+import { createHumanoid } from "./entity.js";
 
 const CONFIG = {
   worldSize: 180,
@@ -65,68 +66,6 @@ let terrainHeightAt, mapState;
 
 const inputController = createInputController();
 const clock = new THREE.Clock();
-
-function makeMaterial(color, roughness = 0.86) {
-  return new THREE.MeshStandardMaterial({ color, roughness, metalness: 0.04 });
-}
-
-/**
- * NPCとプレイヤーで共有する人型モデルを生成します。
- * モデル生成はキャラクターの見た目に関する処理なので、
- * プレイヤーの物理・操作ロジックとは分離したままgame.jsに残します。
- */
-function createHumanoid(options = {}) {
-  const group = new THREE.Group();
-  const skin = makeMaterial(options.skin ?? 0xb98468);
-  const shirt = makeMaterial(options.shirt ?? 0x536a78);
-  const pants = makeMaterial(options.pants ?? 0x34383e);
-  const shoe = makeMaterial(0x202225);
-
-  const torso = new THREE.Mesh(new THREE.CapsuleGeometry(0.38, 0.62, 5, 10), shirt);
-  torso.position.y = 1.18;
-  group.add(torso);
-
-  const neck = new THREE.Mesh(new THREE.CylinderGeometry(0.11, 0.13, 0.16, 8), skin);
-  neck.position.y = 1.75;
-  group.add(neck);
-
-  const head = new THREE.Mesh(new THREE.SphereGeometry(0.29, 14, 10), skin);
-  head.position.y = 2.03;
-  group.add(head);
-
-  function limb(radius, length, material, x, y) {
-    const pivot = new THREE.Group();
-    pivot.position.set(x, y, 0);
-    const mesh = new THREE.Mesh(new THREE.CapsuleGeometry(radius, length, 4, 8), material);
-    mesh.position.y = -length * 0.5;
-    pivot.add(mesh);
-    group.add(pivot);
-    return pivot;
-  }
-
-  const upperArmL = limb(0.105, 0.42, shirt, -0.43, 1.48);
-  const upperArmR = limb(0.105, 0.42, shirt, 0.43, 1.48);
-  const foreArmL = limb(0.095, 0.40, skin, -0.43, 1.06);
-  const foreArmR = limb(0.095, 0.40, skin, 0.43, 1.06);
-  const thighL = limb(0.14, 0.48, pants, -0.19, 0.76);
-  const thighR = limb(0.14, 0.48, pants, 0.19, 0.76);
-  const shinL = limb(0.105, 0.52, pants, -0.19, 0.28);
-  const shinR = limb(0.105, 0.52, pants, 0.19, 0.28);
-
-  const footL = new THREE.Mesh(new THREE.BoxGeometry(0.19, 0.12, 0.34), shoe);
-  footL.position.set(-0.19, 0.05, 0.08);
-  group.add(footL);
-  const footR = footL.clone();
-  footR.position.x = 0.19;
-  group.add(footR);
-
-  group.userData.limbs = {
-    upperArmL, upperArmR, foreArmL, foreArmR,
-    thighL, thighR, shinL, shinR
-  };
-  group.userData.phase = Math.random() * Math.PI * 2;
-  return group;
-}
 
 function setupScene() {
   scene = new THREE.Scene();
